6fa392549ba229b01d9f3c0b15f05e415d9c40ce
Kostia <kostikolesnik2513@gmail.com>
2024-01-19 21:03:11 +0200

    created basic fuctional

--- main.js
+++ main.js
@@ -11,10 +11,41 @@ const nameOfCardInput = document.querySelector('.name-input')
 const textInput = document.querySelector('.text-input')
 const cardsBlock = document.querySelector('.cards')
 
+
+const createCard = (title, text) => {
+    const cardBlock = document.createElement('div')
+    const cardTitle = document.createElement('h3')
+    const cardText = document.createElement('p')
+    
+    cardBlock.className = 'card'
+    cardTitle.textContent = title 
+    cardText.textContent = text
+    
+    cardBlock.append(cardTitle, cardText)
+    
+    return cardBlock
+}
+
+const errorMessage = (text) => {
+    const errorSpan = document.createElement('span')
+    errorSpan.className = 'error-message'
+    errorSpan.textContent = text
+    
+    return errorSpan
+}
+
+
 getForm.addEventListener('submit', (event) => {
     event.preventDefault()
-    const getName = nameOfCardInput.value
+    const getTitle = nameOfCardInput.value
     const getText = textInput.value
-
-    console.log(getName, getText);
+    
+    if(getTitle && getText) {
+        const createdCardItem = createCard(getTitle, getText)
+        cardsBlock.append(createdCardItem)
+    } else {
+        const block = getForm.closest('.create-input')
+        block.append(errorMessage('Could you input corect data'))
+    }
 })
+
